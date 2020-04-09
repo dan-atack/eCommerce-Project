3139e0de6d22374709512e53379abdde88a564e7
Craig Nockels <cnockels@gmail.com>
2020-04-09 14:41:30 -0400

    added globalstyles css reset, start app.js framework

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,17 +1,59 @@
 import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+
+
 
 function App() {
-  const [featured, setFeatured] = useState([]);
 
-  useEffect(() => {
-    fetch('/homePage')
-      .then(res => res.json())
-      .then(data => setFeatured(data));
-  }, []);
 
-  return <>{featured.map(item => {
-    return <div> {item.name} </div>
-  })}</>
-}
+  return (
+    <PageStructure>
+      <HeaderWrap>
+
+      </HeaderWrap>
+
+      <FilterBarWrap>
+
+      </FilterBarWrap>
+
+      <MainWrap>
+        <div>SearchBar</div>
+        <div>ItemDisplay</div>
+      </MainWrap>
+
+      <CheckoutBarWrap>
+
+      </CheckoutBarWrap>
+
+      <FooterWrap>
+
+      </FooterWrap>
+    </PageStructure>
+  );
+};
+
+const PageStructure = styled.div`
+  /* height:100vw; 
+  width:100vh; */
+  position: relative;
+  display: grid;
+
+`;
+const HeaderWrap = styled.div`
+
+`;
+const FilterBarWrap = styled.div`
+
+`;
+const MainWrap = styled.div`
+
+`;
+const CheckoutBarWrap = styled.div`
+
+`;
+const FooterWrap = styled.div`
+
+`;
+
 
 export default App;
