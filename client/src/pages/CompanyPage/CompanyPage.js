413bef11ac48fd31ea392316d128d53975963ec3
Rony Kordahi <kordrony@gmail.com>
2020-04-16 10:43:44 -0400

    pulling

--- client/src/pages/CompanyPage/CompanyPage.js
+++ client/src/pages/CompanyPage/CompanyPage.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import styled from 'styled-components';
 import { useDispatch } from 'react-redux';
 import { searchResults, setDisplayItems } from '../../actions';
 
@@ -15,16 +14,19 @@ function CompanyPage() {
   const dispatch = useDispatch();
 
   useEffect(() => {
+    // fetches the company's name purely to display it
     fetch(`/companyName/${companyId}`)
       .then((res) => res.json())
       .then((data) => setName(data.companyName))
       .then(
+        // fetches the items sold by that company
         fetch(`/products/sort/${companyId}`)
           .then((res) => res.json())
           .then((data) => setCompanyItems(data))
       );
   }, [companyId]);
 
+  // sets the state to be the company's items
   useEffect(() => {
     dispatch(searchResults(companyItems));
     dispatch(setDisplayItems(companyItems));
@@ -33,15 +35,9 @@ function CompanyPage() {
   return (
     <>
       <h2>All products by {name} :</h2>
-      <CompanyProducts>
           <ItemDisplay />
-      </CompanyProducts>
     </>
   );
 }
-const CompanyProducts = styled.div`
-  display: flex;
-  justify-content: space-evenly;
-  flex-wrap: wrap;
-`;
+
 export default CompanyPage;
