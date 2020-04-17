579b7a7f2b2cb5c5b2e6ac965b1592fe73b50f0f
Craig Nockels <cnockels@gmail.com>
2020-04-17 15:45:22 -0400

    clear garbage code

--- client/src/components/ItemSorter/ItemSorter.js
+++ client/src/components/ItemSorter/ItemSorter.js
@@ -1,21 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { sortItems } from '../../actions';
 
 const ItemSorter = () => {
   const dispatch = useDispatch();
-
-  const [selectedOption, setSelectedOption] = React.useState('none');
-
-  const displayItems = useSelector(state => state.filters.displayItems);
-  const baseItems = useSelector((state) => state.filters.baseItems);
-
-  // React.useEffect(() => {
-  //   dispatch(sortItems(selectedOption));
-  // }, [baseItems]);
-
 
   // note option values are stringified so "two" values can be passed
   return (
@@ -25,7 +15,6 @@ const ItemSorter = () => {
         <select
           onChange={(ev) => {
             dispatch(sortItems(ev.target.value));
-            setSelectedOption(ev.target.value);
           }}
         >
           <option value={JSON.stringify({ type: 'none', elv: 'none' })}>
